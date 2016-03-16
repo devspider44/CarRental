9d7feabac12ad9d4d26099152f73050f1beef25d
devSpider <lee.gutierrez44@gmail.com>
2016-03-16 10:27:52 +0800

    Define Business and Client Car Properties

---
+++ CarRental.Business.Entities/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace CarRental.Business.Entities
+{
+    [DataContract (Namespace="http://wwww.pluralsight.com/carrental")]
+    public class Car
+    {
+        [DataMember]
+        public int CardId { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public string Color { get; set; }
+        [DataMember]
+        public int Year { get; set; }
+        [DataMember]
+        public decimal RentalPrice { get; set; }
+        [DataMember]
+        public bool CurrentlyRented { get; set; }
+    }
+}
